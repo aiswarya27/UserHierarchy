179725af77dec584747f6c894af82941dd201e8f
aiswarya27 <aiswaryarajendran@gmail.com>
2021-05-12 09:59:21 +0530

    Add project files.

---
+++ UsersHierarchy.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsersHierarchy.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
